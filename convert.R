51b23da90712f98190d44af845b22f81a1f31ee5
Filip Schouwenaars <filip@datacamp.com>
2016-05-05 16:55:08 +0200

    add library call to load stringr

--- convert.R
+++ convert.R
@@ -1,7 +1,9 @@
 # This little script converts \u____ encodings to &#____; encodings
 file <- "chapter1.Rmd"
+
+library(stringr)
 file_string <- paste(readLines(file), collapse = "\n")
-unicodes <- unique(stringr::str_extract_all(file_string, "\\\\u[0-9a-fA-F]{4}"))[[1]]
+unicodes <- unique(str_extract_all(file_string, "\\\\u[0-9a-fA-F]{4}"))[[1]]
 translations <- paste0("&#", strtoi(gsub("\\\\u", "", unicodes) , base = 16L), ";")
 names(translations) <- paste0("\\", unicodes)
 write(str_replace_all(file_string, translations), file = "chapter1.Rmd")
